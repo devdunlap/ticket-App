63b0633b6f2454ba153348bfa0ad6164a8f6763b
devdunlap <108426486+devdunlap@users.noreply.github.com>
2023-04-04 14:32:20 -0500

    created increase and decrease button

--- src/Components/TicketApp/TicketApp.jsx
+++ src/Components/TicketApp/TicketApp.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 // import team from '../../data/team.json'
 
 
-
 const TicketApp = (props) => {
+  
   const { team } = props
   
+  const [count, setCount] = useState(0)
+
+  const handleClickIncrease = () => {
+    setCount(count + 1)
+  }
+
+  const handleClickDecrease = () => {
+    setCount(count - 1)
+  }
+  
+
   const mappedTeam = team.map((member) => {
+    const { name, role } = member
     return (
     <div>
-    <p>{member.name}</p>
-    <p>{member.role}</p>
+    <h1>{name}</h1>
+    <p>{role}</p>
+    <p>{count}</p>
+    <button onClick={handleClickIncrease}>+</button>
+    <button onClick={handleClickDecrease}>-</button>
     </div>
     )
   })
+
+
   
     return (
     <div>
